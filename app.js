51d6919acadbf06af7f61961d7296d61b1d92fa3
david-guadalupe <guadalupedavid36@gmail.com>
2024-09-16 17:41:16 -0400

    included api call to unsplash api to get images. created search function to display results and clear when a new search term is entered

--- app.js
+++ app.js
@@ -10,12 +10,28 @@ let page = 1;
 
 async function searchImages() {
     keyword = searchBox.value;
-    const url = 'https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}';
+    const url = 'https://api.unsplash.com/search/photos?page=';
 
-    const response = await fetch(url);
+    const response = await fetch(url + page + '&query=' + keyword + '&client_id=' + accessKey + '&per_page=12');
     const data = await response.json();
 
-    console.log(data);
+    const results = data.results;
+    if(page == 1){
+        searchResult.innerHTML = '';
+    }
+    results.map((results) =>{
+        const image = document.createElement("img");
+        image.src = results.urls.small;
+        const imageLink = document.createElement("a");
+        imageLink.href = results.links.html;
+        imageLink.target= "_blank";
+
+        imageLink.appendChild(image);
+        searchResult.appendChild(imageLink);
+    })
+    showMoreButton.style.display = 'block';
+
+    // console.log(data);
 }
 
 searchForm.addEventListener("submit", (e) => {
@@ -23,3 +39,8 @@ searchForm.addEventListener("submit", (e) => {
     page = 1;
     searchImages();
 });
+
+showMoreButton.addEventListener('click', () => {
+    page++;
+    searchImages();
+})
